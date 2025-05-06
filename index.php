96addc09cd1e02104d1fd18941e11a0bc3add12b
Irgivensa12 <159887639+Irgivensa12@users.noreply.github.com>
2025-05-06 08:03:54 +0700

    Tambah  header, foto, dan form di index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <h2>Kristoforus Irgivensa Arielino</h2>
+        <h3>235314022</h3>
+        <img src="AkuPDD.jpg" alt="Diriku" width="100">
+    </header>
+
+    <h1>To Do List</h1>
+    <form action="" method="post">
+        <input type="text" name="tugas" placeholder="Tugas baru" required>
+        <button type="submit" name="tambah">Tambah</button>
+    </form>
+</body>
+</html>
